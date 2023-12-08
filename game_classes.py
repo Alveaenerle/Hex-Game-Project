b58f5a79be4c4ad9212a885788a2c85262c25df6
Dominik Śledziewski <01187296@pw.edu.pl>
2023-12-08 15:04:34 +0100

    Created game_classes.py Added framework to it

---
+++ game_classes.py
@@ -0,0 +1,16 @@
+from two_player_games.game import Game
+from two_player_games.move import Move
+from two_player_games.player import Player
+from two_player_games.state import State
+
+
+class Hex(Game):
+    pass
+
+
+class HexState(State):
+    pass
+
+
+class HexMove(Move):
+    pass
